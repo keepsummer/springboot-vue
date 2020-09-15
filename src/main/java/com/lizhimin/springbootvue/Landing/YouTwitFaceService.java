fc29ca452220597ad2471101cd3c24a4f6b13399
李志敏 <915377975@qq.COM>
2020-09-15 13:45:57 +0800

    refactor： 对redis中的key进行封装

--- src/main/java/com/lizhimin/springbootvue/Landing/YouTwitFaceService.java
+++ src/main/java/com/lizhimin/springbootvue/Landing/YouTwitFaceService.java
@@ -7,7 +7,6 @@ import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.SessionCallback;
 import org.springframework.stereotype.Service;
-import sun.awt.windows.ThemeReader;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -15,9 +14,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- *一个虚拟社交网站，用户销售商品
- */
 @Service
 public class YouTwitFaceService {
     @Autowired
@@ -26,15 +22,19 @@ public class YouTwitFaceService {
     RedisTemplate redisTemplate;
 
     private static final Long SIX_SECONDS = 6000L;
-    private static final String MARKET = "market:";
+    private static final String MARKET_KEY = "market:";
+    private static final String NAME_KEY = "name";
+    private static final String FUNDS_KEY = "funds";
+    private static final String INVENTORY_KEY = "inventory:";
+    private static final String USER_KEY = "user:";
     /** HashSet
      * 添加用户信息
      */
     public Boolean addUserInfo(String userId, String name, Double funds){
         Map<String,Object> userInfoMap = new HashMap<>(10);
-        userInfoMap.put("name",name);
-        userInfoMap.put("funds",funds);
-        return redisUtil.hmset("user:"+userId,userInfoMap);
+        userInfoMap.put(NAME_KEY,name);
+        userInfoMap.put(FUNDS_KEY,funds);
+        return redisUtil.hmset(USER_KEY+userId,userInfoMap);
     }
 
     /** set
@@ -42,7 +42,7 @@ public class YouTwitFaceService {
      * @return 成功
      */
     public Long addInventory(String userId,String itemId){
-      return redisUtil.sSet("inventory"+":"+userId,itemId);
+      return redisUtil.sSet(INVENTORY_KEY+userId,itemId);
     }
 
     /**
@@ -54,7 +54,7 @@ public class YouTwitFaceService {
      */
     public Boolean addItemInfo(String itemId,String userId,Double price){
 
-        return redisUtil.zset("market:",itemId+":"+userId,price);
+        return redisUtil.zset(MARKET_KEY,itemId+":"+userId,price);
     }
     /**
      * 添加商品到市场中去 加事务，
@@ -65,7 +65,7 @@ public class YouTwitFaceService {
      */
     public Boolean addItemInfoToMarket(String itemId,String userId,Double price){
         //1、定义：inventory
-        String inventory = "inventory"+":"+userId;
+        String inventory = INVENTORY_KEY+userId;
         //获取当前时间
         long l = System.currentTimeMillis();
         while (System.currentTimeMillis()-l <= SIX_SECONDS){
@@ -85,7 +85,7 @@ public class YouTwitFaceService {
                 public Object execute(RedisOperations operations) throws DataAccessException {
                     operations.multi();
                     System.out.println("事务开始");
-                    operations.opsForZSet().add("market:",itemId+":"+userId,price);
+                    operations.opsForZSet().add(MARKET_KEY,itemId+":"+userId,price);
                     operations.opsForSet().remove(inventory, itemId);
                     System.out.println("itemId: "+itemId);
                     System.out.println("price: "+price);
@@ -102,21 +102,22 @@ public class YouTwitFaceService {
         //2、获取买家拥有的钱和商品的售价
         //3、检查买家的钱是否足够
         //4、扣减买家的钱，增加卖家的钱，买家的背包增加该商品，市场上减少该商品
-        String buyer = "user"+":"+buyerId;
-        String seller = "user"+":"+sellerId;
+        String buyer = USER_KEY+buyerId;
+        String seller = USER_KEY+sellerId;
         String item = itemId + ":" + sellerId;
-        String inventory = "inventory"+":"+buyerId;
+        String inventory = INVENTORY_KEY+buyerId;
         List<String> keyList = new ArrayList<>();
-        keyList.add("market:");
+        keyList.add(MARKET_KEY);
         keyList.add(buyer);
 
             long l = System.currentTimeMillis();
+
             while (System.currentTimeMillis()-l <= SIX_SECONDS){
                 try {
                     redisTemplate.watch(keyList);
                     //获取商品的售价
-                    Double price1 = redisUtil.zscore("market:",item);
-                    Object funds = redisUtil.hget(buyer, "funds");
+                    Double price1 = redisUtil.zscore(MARKET_KEY,item);
+                    Object funds = redisUtil.hget(buyer, FUNDS_KEY);
                     if(funds instanceof BigDecimal){
                         BigDecimal funds1= (BigDecimal) funds;
                         if(funds1.doubleValue()< price && !price.equals(price1)){
@@ -128,10 +129,10 @@ public class YouTwitFaceService {
                           public Object execute(RedisOperations operations) throws DataAccessException {
                               operations.multi();
                               //先将买家的钱支付给买家，
-                              operations.opsForHash().increment(buyer,"funds",-price);
-                              operations.opsForHash().increment(seller,"funds",price);
+                              operations.opsForHash().increment(buyer,FUNDS_KEY,-price);
+                              operations.opsForHash().increment(seller,FUNDS_KEY,price);
                               operations.opsForSet().add(inventory,itemId);
-                              operations.opsForZSet().remove("market:",item);
+                              operations.opsForZSet().remove(MARKET_KEY,item);
                               operations.exec();
                               return true;
                           }
@@ -140,7 +141,6 @@ public class YouTwitFaceService {
                     }
                 }catch (Exception e){
                     System.out.println("出现异常");
-                    continue;
                 }
             }
 
